08ca321c346bfc65fd9f0b5bdbfc5480787c7114
Sabre <1993sabre@gmail.com>
2022-11-13 23:39:20 +0900

    edit

--- app.py
+++ app.py
@@ -27,39 +27,44 @@ st.write(pd.DataFrame(model.coef_[0], index=["pclass", "age", "sex", "sex*pclass
 
 st.write("---")
 
-pclass = st.selectbox(
-    label="객실등급", # 상단 표시되는 이름,
+# 첫번째 행
+r1_col1, r1_col2 = st.columns(2)
+
+# 객실 등급 입력 (셀렉트 박스)
+pclass = r1_col1.selectbox(
+    label="객실 등급", # 상단 표시되는 이름,
     options=[1, 2, 3]
 )
 
-with st.echo(code_location="below"):
-    # 나이 입력 (숫자)
-    age = st.number_input(
-        label="나이", # 상단 표시되는 이름
-        min_value=0, # 최솟값
-        max_value=99, # 최댓값
-        step=1, # 입력 단위
-        # value=30 # 기본값
-    )
+# 나이 입력 (숫자 입력)
+age = r1_col2.number_input(
+    label="나이", # 상단 표시되는 이름
+    min_value=0, # 최솟값
+    max_value=99, # 최댓값
+    step=1, # 입력 단위
+    #value=30 # 기본값
+)
+
+# 두번째 행
+r2_col1, r2_col2 = st.columns(2)
     
-with st.echo(code_location="below"):
-    # 성별 입력 (라디오 버튼)
-    sex_button = st.radio(
-        label="성별", # 상단 표시되는 이름
-        options=["남성", "여성"], # 선택 옵션
-        # index=0 # 기본 선택 인덱스
-        # horizontal=True # 가로 표시 여부
-    )
+# 성별 입력 (라디오 버튼)
+sex_button = r2_col1.radio(
+    label="성별", # 상단 표시되는 이름
+    options=["남성", "여성"], # 선택 옵션
+    # index=0 # 기본 선택 인덱스
+    # horizontal=True # 가로 표시 여부
+)
     
 if sex_button == "남성" :
     sex = 0
 else :
     sex = 1
-
-with st.echo(code_location="below"):
-    cabin_class_button = st.radio(
-        label="객실번호",
-        options=["있음", "없음"]
+     
+# 객실 번호 입력 (라디오 버튼)
+cabin_class_button = st.radio(
+    label="객실 번호",
+    options=["있음", "없음"]
     )
 
 if cabin_class_button == "있음" :
@@ -67,23 +72,23 @@ if cabin_class_button == "있음" :
 else :
     cabin_class = 0
     
-with st.echo(code_location="below"):
-    # 실행 버튼
-    play_button = st.button(
-        label="예측", # 버튼 내부 표시되는 이름
+# 실행 버튼
+play_button = st.button(
+    label="예측", # 버튼 내부 표시되는 이름
     )
 
 st.write("---") # 구분선
 
-with st.echo(code_location="below"):
-    # 실행 버튼이 눌리면 모델을 불러와서 예측한다
-    if play_button:
-        st.snow() # 눈송이 애니메이션 표시
-        input_values = [[
-            pclass, age, sex, sex*pclass, cabin_class
+# 실행 버튼이 눌리면 모델을 불러와서 예측한다
+if play_button:
+    input_values = [[
+        pclass, age, sex, sex*pclass, cabin_class
         ]]
-        pred = model.predict(input_values)
-        st.write("## 분류")
-        st.write("생존" if pred[0] == 1 else "사망")
+    pred = model.predict(input_values)
+    
+st.write("---") # 구분선
+
+st.write("## 분류")
+st.write("생존" if pred[0] == 1 else "사망")
 
 
