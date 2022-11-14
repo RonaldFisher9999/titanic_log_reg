3da694f38a7cf81439bf695c87f7223f919174c3
Sabre <1993sabre@gmail.com>
2022-11-14 23:58:35 +0900

    edit

--- app.py
+++ app.py
@@ -124,3 +124,15 @@ with st.echo(code_location="below") :
     odds = np.exp(log_odds)
     p_death = round(((1/(1+odds)) * 100), 2)
     p_surv = round((100 - p_death), 2)
+    
+st.markdown(
+    """
+    ```
+    # 개별 데이터 생존 확률 계산
+    data = [pclass, sex, age, sex*pclass, cabin_class]
+    log_odds = sum(data * model.coef_[0]) + model.intercept_[0]
+    odds = np.exp(log_odds)
+    p_death = round(((1/(1+odds)) * 100), 2)
+    p_surv = round((100 - p_death), 2)
+    ```"""
+    )
