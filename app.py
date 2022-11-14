82df0c57f7ab5a400b845d85080dfe5e33640892
Sabre <1993sabre@gmail.com>
2022-11-14 23:41:05 +0900

    edit

--- app.py
+++ app.py
@@ -89,17 +89,6 @@ play_button = st.button(
 
 st.write("---") # 구분선
 
-st.write("#### 로지스틱 회귀 모델")
-st.write(pd.DataFrame(model.coef_[0], index=["pclass", "sex", "age", "sex*pclass", "cabin_class"], columns=["계수"]))
-
-with st.echo(code_location="below") :
-    # 개별 데이터 생존 확률 계산
-    data = [pclass, sex, age, sex*pclass, cabin_class]
-    log_odds = sum(data * model.coef_[0]) + model.intercept_[0]
-    odds = np.exp(log_odds)
-    p_death = round(((1/(1+odds)) * 100), 2)
-    p_surv = round((100 - p_death), 2)
-
 # 실행 버튼이 눌리면 모델을 불러와서 예측한다
 if play_button :
     input_values = [[
@@ -114,3 +103,16 @@ if play_button :
         st.write("## 사망")
         st.write(f"{p_surv}% 확률로 생존, {p_death}% 확률로 사망")
     get_image("death.jfif")
+    
+st.write("---")
+    
+st.write("#### 로지스틱 회귀 모델")
+st.write(pd.DataFrame(model.coef_[0], index=["pclass", "sex", "age", "sex*pclass", "cabin_class"], columns=["계수"]))
+
+with st.echo(code_location="below") :
+    # 개별 데이터 생존 확률 계산
+    data = [pclass, sex, age, sex*pclass, cabin_class]
+    log_odds = sum(data * model.coef_[0]) + model.intercept_[0]
+    odds = np.exp(log_odds)
+    p_death = round(((1/(1+odds)) * 100), 2)
+    p_surv = round((100 - p_death), 2)
