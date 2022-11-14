e4044d334ac2e2fd986be0173a3193c34de96edd
Sabre <1993sabre@gmail.com>
2022-11-14 23:59:31 +0900

    edit

--- app.py
+++ app.py
@@ -116,15 +116,7 @@ st.write("---")
     
 st.write("#### 로지스틱 회귀 모델")
 st.write(pd.DataFrame(model.coef_[0], index=["pclass", "sex", "age", "sex*pclass", "cabin_class"], columns=["계수"]).T)
-
-with st.echo(code_location="below") :
-    # 개별 데이터 생존 확률 계산
-    data = [pclass, sex, age, sex*pclass, cabin_class]
-    log_odds = sum(data * model.coef_[0]) + model.intercept_[0]
-    odds = np.exp(log_odds)
-    p_death = round(((1/(1+odds)) * 100), 2)
-    p_surv = round((100 - p_death), 2)
-    
+   
 st.markdown(
     """
     ```
